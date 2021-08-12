a6cdd18dc5c93563108a18744e505db037e7e6de
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-12 13:05:08 +0200

    3

---
+++ Concatenacion/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace Concatenacion
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int edad = 19;
+            //LA INTERPOLACION ES UNICA A C# PARA CONCATENAR, SE IMPLEMENTA
+            // INSERTANDO EL SIGNO "$" ANTES DE LAS COMILLAS Y LLAMANDO A LA VARIABLE DENTRO DE LLAVES.
+
+            Console.WriteLine($"Tu edad es igual a : {edad}"); // ES LA MANERA DE IMPLEMENTAR LA INTERPOLACION
+        }
+    }
+}
