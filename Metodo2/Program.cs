a74750ac275b37522ce824fbe2841c026cf86561
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-17 18:19:26 +0200

    llamado a metodos con flecha

---
+++ Metodo2/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace Metodo2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+        }
+
+        void primerMetodo()
+        {
+            int number1 = 8;
+
+            int number2 = 7;
+
+            Console.WriteLine(number1 + number2);
+        }
+
+
+        void segundoMetodo()
+        {
+            Console.WriteLine();
+        }
+    }
+}
