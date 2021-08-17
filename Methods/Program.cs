75b16f828b20c1000994888015c2f380c3140958
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-17 18:21:19 +0200

    flechado

--- Methods/Program.cs
+++ Methods/Program.cs
@@ -4,6 +4,10 @@ namespace Methods
 {
     class Program
     {
+
+
+        int numero1 = 5;
+        int numero2 = 9;
         static void Main(string[] args)
         {
             mensajeEnPantalla();
@@ -15,8 +19,16 @@ namespace Methods
             Console.WriteLine("introduce el numero 2");
             int num2 = int.Parse(Console.ReadLine());
             Console.WriteLine(sumaNumeros(num1, num2)); //LLAMADO AL METODO CON RETURN 
-            Console.WriteLine($"la division de los numeros es igual a {divideNumber(num1, num2)}");
-        
+            Console.WriteLine($"la division de los numeros es igual a {divideNumber(num1,num2)}");
+
+
+            Console.WriteLine("introduce la letra ");
+            string a = Console.ReadLine();
+
+            Console.WriteLine("introduce la letra ");
+            string b = Console.ReadLine();
+
+            Console.WriteLine($"la suma de letras es {gimeName(a,b)}");
         }
 
         static void mensajeEnPantalla()
@@ -32,5 +44,15 @@ namespace Methods
         }
 
         static double divideNumber(int num1, int num2) => num1 / num2; // EN C# LA NOMENCLATURA CON LA FLECHA FUNCIONA PARA METODOS QUE SOLO TENGAN UNA LINEA DE EJECUCION PARA SIMPLIFICAR.
+
+        static int suma(int op1, int op2) => op1 * op2;
+
+        static string gimeName(string a, string b) => a + b;
+
+        void segundoMetodo()
+        {
+            Console.WriteLine(numero1 * numero2);
+        }
+    
     }
 }
