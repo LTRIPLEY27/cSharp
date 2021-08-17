6d137aa12228907aa7ad2265ac90530c52ba8c2f
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-17 12:13:04 +0200

    CONSTANTES ADD

--- Concatenacion/Program.cs
+++ Concatenacion/Program.cs
@@ -49,8 +49,39 @@ namespace Concatenacion
             double pesoMaterialPrec = pesoMaterial;
 
             Console.WriteLine(pesoMaterialPrec);
-        
+
+            //////// PARSE ////////////
+            ///
+            /*
+            Console.WriteLine("introduce valor 1");
+
+            Console.ReadLine(); // OBTIENE EL VALOR POR CONSOLA
+            int aux1 = int.Parse(Console.ReadLine()); // CON EL PARSE CONVERTIMOS EL VALOR TEXTO EN NUMERICO
+            Console.WriteLine("introduce valor 2");
+            int aux2 = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("La suma es " + (aux1 + aux2));
+            */
+
+            ////////// CONSTANTES ///////////
+            const int VALOR = 5; // LAS CONSTANTES SE HAN DE DEFINIR EN LA MISMA LINEA QUE SE INICIA
+            // LAS CONSTANTES, PARA DIFERENCIARLAS DE LA VARIABLE, SE HAN DE ASIGNAR EN MAYUSCULAS
+
+            const int VALOR2 = 9;
+                                    //EL USO DE LAS CONSTANTES Y SUS VALORES SE HACEN
+                                    //COMO EN PARAMETROS, LLAMANDO EL VALOR SEGUN SU INDEX, PARTIENDO DESDE 0
+            Console.WriteLine("el valor de la constante es {0}" , VALOR, VALOR2); // EL USO DE LAS CONSTANTES SE PUEDE HACER MEDIANTE "," EN LA CONCATENACION
+
+            const double PI = 3.1416;
+            Console.WriteLine("introduce la medida");
+
+            double radi = double.Parse(Console.ReadLine());
+
+            // double area = radi * radi * PI;
+            double area = Math.Pow(radi, 2)*PI; // EL METODO POW TOMA COMO ARGUMENTOS EL PARAMETRO * EL EXPONENTE AL QUE SE DESEA PROYECTAR
+            Console.WriteLine($"el area del circulo es : {area}");
         }
+
     }
     
 }
