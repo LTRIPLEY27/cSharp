e417819e2d2e4fc26669de3c6991e25cf6c8797d
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-19 17:18:21 +0200

    IF inicio

---
+++ CondicionalIF/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace CondicionalIF
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*
+                bool haceFrio;
+
+                haceFrio = true;
+
+                Console.WriteLine(haceFrio);*/
+
+            int edad;
+            Console.WriteLine("evaluaremos si eres mayor o no");
+            Console.WriteLine("inserte su edad");
+            edad = int.Parse(Console.ReadLine());
+
+            if(edad >= 18)
+            {
+                Console.WriteLine("eres mayor");
+            } else
+            {
+                Console.WriteLine("no eres mayor");
+            }
+        }
+    }
+}
