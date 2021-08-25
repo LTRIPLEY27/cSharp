c510c413b4a40face377f4bb6fa6e739f0194edf
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-25 17:23:50 +0200

    prove the if conditional

--- CondicionalIF/Program.cs
+++ CondicionalIF/Program.cs
@@ -11,7 +11,7 @@ namespace CondicionalIF
 
                 haceFrio = true;
 
-                Console.WriteLine(haceFrio);*/
+                Console.WriteLine(haceFrio);
 
             int edad;
             Console.WriteLine("evaluaremos si eres mayor o no");
@@ -24,7 +24,65 @@ namespace CondicionalIF
             } else
             {
                 Console.WriteLine("no eres mayor");
+            }*/
+            int edad;
+
+            Console.WriteLine("evaluaremos si puedes conducir");
+
+            Console.WriteLine("indique su edad?");
+            edad = int.Parse(Console.ReadLine());
+
+            bool carnet = true;
+
+            /*
+            if(edad < 18)
+            {
+                Console.WriteLine("lo siento, estoy notificando a la policia no puedes conducir");
+                carnet = false;
+                Console.WriteLine(carnet);
+            } else
+            {
+                carnet = true;
+                Console.WriteLine("puedes conducir");
+                Console.WriteLine(carnet);
+            }*/
+
+
+            // IF ANIDADOS
+
+            /* PROGRAMA QUE EVALUA LA EDAD Y EL CARNET PARA CONDUCIR, EN CASO DE UNA NEGACION CIERRA 
+             */
+
+            //int edad;
+            string response;
+            
+
+            Console.WriteLine("evaluaremos si puedes conducir");
+
+            Console.WriteLine("indique su edad?");
+            edad = int.Parse(Console.ReadLine());
+
+            
+
+            if(edad >= 18 )
+            {
+            Console.WriteLine("posee carnet de conducir");
+            response = Console.ReadLine();
+                if(response == "si")
+                {
+                    Console.WriteLine("puedes conducir");
+                } else
+                {
+                    Console.WriteLine("No puedes conducir asi seas mayor de edad es necesario dispongas d eun carnet");
+                }
+                
+            } else
+            {
+                Console.WriteLine("No puedes ya que eres menor de edad");
             }
+
+
+
         }
     }
 }
