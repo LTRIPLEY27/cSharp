db08801ebe3fe9729a162651541c8c8518508259
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-30 13:28:43 +0200

    while

---
+++ BucleWhile/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace BucleWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Iniciar bucle?");
+            string resp = Console.ReadLine();
+
+            while(resp != "no")
+            {
+                Console.WriteLine("INICIO BUCLE WHILE");
+            }  
+            //
+
+
+        }
+    }
+}
