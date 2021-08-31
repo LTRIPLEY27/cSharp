63b3650edbd7a6b7cffe87fe98764adab87e1b32
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-08-31 13:07:22 +0200

    version + contador

--- bucleWhile1/Program.cs
+++ bucleWhile1/Program.cs
@@ -10,33 +10,37 @@ namespace bucleWhile1
             Random number = new Random();
 
             int aleat = number.Next(0, 100);
+            int cont = 0;
 
             Console.WriteLine(aleat);
 
             Console.WriteLine("Introduzca el numero a verificar?");
 
             int num = int.Parse(Console.ReadLine());
+            cont++;
 
             while (num != aleat)
             {
                 if(num < aleat)
                 {
-                    Console.WriteLine($"el numero a hallr es mayor a {num}, intentelo de nuevo");
+                    Console.WriteLine($"el numero a hallar es mayor a {num}, intentelo de nuevo");
                     Console.WriteLine("Introduzca el numero a verificar?");
 
                     num = int.Parse(Console.ReadLine());
+                    cont++;
                 } else
                 {
                     Console.WriteLine($"el numero a hallar es menor a {num}, intentelo de nuevo");
                     Console.WriteLine("Introduzca el numero a verificar?");
 
                     num = int.Parse(Console.ReadLine());
+                    cont++;
                 }
                 
                 
             }
 
-            Console.WriteLine($"Has acertado, el numero era {num}");
+            Console.WriteLine($"Has acertado, el numero era {num}, el numero de intento que te ha costado han sido: {cont}");
             
 
             /*Programa numero aleatorio entre 0 y 100*/
