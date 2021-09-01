516089206690a8a18b5c19267b809f6582327693
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-01 11:34:49 +0200

    Lanzamiento de excepciones THROW

---
+++ LanzamientoThrow/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+namespace LanzamientoThrow
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Indique el mes a informar");
+            int mes = int.Parse(Console.ReadLine());
+
+            try
+                {
+                Console.WriteLine(NameMonth(mes));
+            } catch (Exception e)
+            {
+                Console.WriteLine("numero incorrecto" + e.Message);
+            }
+            
+        }
+
+        public static string NameMonth(int mes)
+        {
+            switch(mes)
+            {
+                case 1:
+                    return "January";
+                case 2:
+                    return "February";
+                case 3:
+                    return "March";
+                case 4:
+                    return "April";
+                case 5:
+                    return "May";
+                case 6:
+                    return "June";
+                case 7:
+                    return "July";
+                case 8:
+                    return "August";
+                case 9:
+                    return "September";
+                case 10:
+                    return "Octuber";
+                case 11:
+                    return "November";
+                case 12:
+                    return "December";
+                default:
+                    // return "Inexistent Month";
+                    throw new ArgumentOutOfRangeException();// CREACION DEL OBJETO DEL UMENT EXCEPTIONTIPO ARG
+                //CON LA EXCEPCION "throw" SE OBLIGA A HACER USO DEL "try catch" EN LA LLAMADA DEL METODO
+            }
+        }
+
+    }
+}
---
+++ checked/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace UsoChecked{
+    class Program
+{
+    static void Main(string[] args)
+    {
+            
+                int num = int.MaxValue; // EL METODO "MaxValue" REFIERE AL NUMERO MAXIMO DE UN PRIMITIVO INT
+
+            //int result = checked(num + 20); // FORMA ABREVIADA DEL CHECKED PARA VERIFICAR UNA COMPROBACION ESPECIFICA
+
+                int result = unchecked(num + 15); // ANVERSO DEL CHECKED
+                Console.WriteLine(result);
+           
+            
+    }
+}
+}
