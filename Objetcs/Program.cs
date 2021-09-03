68c5bf7b79ebe8753d8e3892e9288b692093e854
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-03 12:34:05 +0200

    Objetos

---
+++ Objetcs/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Objetcs
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("hello stranger");
+        }
+    }
+}
