b5005139380f14ece6302f6c5ef189a7d19bba89
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-09 12:00:09 +0200

    encapsulacion

--- Objetcs/Program.cs
+++ Objetcs/Program.cs
@@ -6,7 +6,68 @@ namespace Objetcs
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("hello stranger");
+
+            Circle newCircle = new Circle(); // CREACION DE OBJETO E INICIACION DE LA CLASE
+
+
+            Console.WriteLine("hello stranger, indique el radio");
+            int radio = int.Parse(Console.ReadLine());
+
+            newCircle.CalculaArea(radio);
+
+            //Console.WriteLine(newCircle.pi); SI NO ES ACCESIBLE NO SE MODIFICA, POR ENDE SE DECLARA COMO CONSTANTE
+            
+            Console.WriteLine($"El area es igual a {newCircle.CalculaArea(radio)}");
+
+            Converssor obj1 = new Converssor();
+
+            obj1.CambiaValor(1.37);
+
+            Console.WriteLine(obj1.Convertir(55));
+
+            Console.ReadKey();
         }
     }
+
+    // PRIMERA CLASE CREADA EN CSHARP (SIEMPRE DENTRO DEL NAMESPACE)
+    class Circle
+    {
+
+        const double pi = 3.1416; // PROPIEDAD CLASE CIRCULO
+
+        
+        public double CalculaArea(int radio) // METODO DE CLASE
+        {
+            return pi * radio * radio;
+        }
+
+    }
+
+    class Converssor
+    {
+        private double euro = 1.23;
+
+        public double Convertir(double cant)
+        {
+            return cant * euro;
+        }
+
+
+        public void CambiaValor(double nuevo)
+        {
+            if(nuevo < 0) 
+            {
+                euro = 1.23;
+
+            }else
+            {
+                euro = nuevo;
+            }
+
+            
+                
+        }
+
+    }
+
 }
