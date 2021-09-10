b19ba92b2ab5690e35379857a67decf119f5f025
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-10 17:47:05 +0200

    Arrys con char recorriendo el alfabeto

--- ResoluciondoWhile/Program.cs
+++ ResoluciondoWhile/Program.cs
@@ -40,17 +40,40 @@ namespace ResoluciondoWhile
                 }
             }
 
-            while (k < 3) 
+            while (k < 5) 
             {
-                Console.WriteLine($"vamos por aca");
+               // Console.WriteLine($"vamos por aca {k}");
                 if(k == 2)
                 {
                     Console.WriteLine("soy penultimo {0} elemento, ya puedes dilucidar la longitud", k);
                     k++;
                     continue;
-                    
+                   
                 }
                 k++;
+
+                Console.WriteLine(k);
+            }
+
+            for(int m = 0; m < 5; m++)
+            {
+                if (m == 3)
+                {
+                    continue;
+                }
+
+                Console.WriteLine("la iteracion " + m);
+            }
+
+
+            // BREAK : ROMPE EL CICLO
+            // CONTINUE: HACE EL SALTO DE LA LINEA DONDE SE LE ESPECIFIQUE LA CONDICION
+
+            for (int i = 0; i < 6; i++)
+            {
+                if(i == 3) { break; }
+                Console.WriteLine("iteracion {0}", i);
+
             }
 
 
