a32fdb662281b4a0980593772019ac1eb1e25609
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-15 20:50:14 +0200

    Recursividad

---
+++ Recursividad/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recursividad
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            int num = Convert.ToInt16(Console.ReadLine());
+            Console.WriteLine("la suma del numero y de sus sucesivos..." + Calculo(num));
+
+
+            Console.ReadKey();
+        }
+
+        static int Calculo(int n)
+        {
+
+            if(n == 1)
+            {
+                return 1;
+               
+            }
+
+            Console.WriteLine("el valor de m es " + n);
+            return n + Calculo(n - 1);
+        }
+    }
+}
