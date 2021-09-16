cee001d891015ed4180235744a9fa2bba34dcd2a
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-16 18:22:32 +0200

    Sobrecarga de constructores con llamados a si mismos. MODULARIZACION

---
+++ ClasesObjetos/ClsEmpleados.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesObjetos
+{
+    class ClsEmpleados // CLASE EMPLEADOS EN OTRA PESTAÑA (MODULARIZACION)
+    {
+        public string Nombre;
+        public float SueldoDiario;
+        public int Edad;
+        
+        
+        public ClsEmpleados(string n, float s, int e) // CONSTRUCTOR
+        {
+            this.Nombre = n;
+            this.SueldoDiario = s;
+            this.Edad = e;
+        }
+
+        // SOBRECARGA DE CONSTRUCTORES
+        public ClsEmpleados()
+        {
+            this.Nombre = "";
+            this.SueldoDiario = 0.0f;
+        }
+        // CONSTRUCTOR QUE RECIBE 2 PARAMETROS ADOPTA AL QUE RECOGE 3 PARAMETROS Y LE INDICA LOS MISMOS VALORES 
+        public ClsEmpleados(string n, int e) : this(n, 850.0f, e) { }  // CONSTRUCTOR STATIC ****  LLAMA A OTRO CONSTRUCTOR 
+
+        public float CalculaSueldo(int NumDias)
+        {
+            return SueldoDiario *= NumDias;
+        }
+
+    }
+}
---
+++ ClasesObjetos/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClasesObjetos
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int dias, age;
+            string name;
+            float sould;
+
+            
+
+            Console.WriteLine("indique su nombre");
+            name = Console.ReadLine();
+
+            //emple.Nombre = name;
+
+            Console.WriteLine("indique su edad");
+            age = Convert.ToInt32(Console.ReadLine());
+
+            //emple.Edad = age;
+
+            Console.WriteLine("indique su saldo");
+            sould = (float)Convert.ToDouble(Console.ReadLine());
+
+            //emple.SueldoDiario = sould;
+
+            ClsEmpleados emple = new ClsEmpleados(name, sould, age);
+
+            ClsEmpleados emple1 = new ClsEmpleados("CAMI", 33);
+
+            Console.WriteLine(emple1.SueldoDiario);
+
+            Console.WriteLine("indique el numero de dias trabajados?");
+            dias = Convert.ToInt16(Console.ReadLine());
+
+            Console.WriteLine($"El sueldo segun sus dias es {emple.CalculaSueldo(dias)}");
+
+
+            Console.ReadKey();
+        }
+    }
+}
