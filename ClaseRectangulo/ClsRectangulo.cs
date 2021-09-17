26fedea226e7e6beea44a4d6e7e235a1de232866
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-17 15:34:49 +0200

    clase rectangulo

---
+++ ClaseRectangulo/ClsRectangulo.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseRectangulo
+{
+    class ClsRectangulo
+    {
+        public int lado1;
+        public int lado2;
+
+        public ClsRectangulo()
+        {
+            this.lado1 = 0;
+            this.lado2 = 0;
+        }
+
+        public ClsRectangulo(int l1, int l2)
+        {
+            this.lado1 = l1;
+            this.lado2 = l2;
+        }
+
+        public int CalculaArea()
+        {
+            return this.lado1 * this.lado2;
+        }
+
+        public int CalculaPerimetro()
+        {
+            return 2 * (this.lado1 + this.lado2);
+        }
+    }
+
+}
---
+++ ClaseRectangulo/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseRectangulo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsRectangulo rect1 = new ClsRectangulo();
+            rect1.lado1 = 12;
+            rect1.lado2 = 10;
+
+            Console.WriteLine("el area del rectangulo es: " + rect1.CalculaArea());
+
+            ClsRectangulo rect2 = new ClsRectangulo(14, 25);
+
+            Console.WriteLine("el area del rectangulo es " + rect2.CalculaPerimetro());
+        }
+    }
+}
