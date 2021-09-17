912beaeda1650e2487284d16f945d191986c7f6c
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-17 16:43:48 +0200

    Encapsulamiento, ejercicio2

---
+++ EncapsulamientoClases/ClsEmpleado.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamientoClases
+{
+    class ClsEmpleado
+    {
+        //PROPIEDADES
+        private string _Nombre;//ENCAPSULAR
+        private int _Edad;
+        private float _SueldoDiario;
+
+        public string Nombre {  // FORMA ABREVIADA DEL GETTER Y SETTER
+            
+            get => _Nombre; 
+            set => _Nombre = value; 
+        }
+        public int Edad { get => _Edad; set => _Edad = value; }
+        public float SueldoDiario { get => _SueldoDiario; set => _SueldoDiario = value; }
+
+        //CONSTRUCTOR
+
+        public ClsEmpleado(string N, int E, float S)
+        {
+            this._Nombre = N;
+            this.Edad = E;
+            this.SueldoDiario = S;
+        }
+
+        public float CalculaSueldo(int NumDias)
+        {
+            //return this._SueldoDiario * NumDias;  // REFIERE A LA PROPIEDAD
+            //return _SueldoDiario * NumDias;// REFIERE A LA PROPIEDAD
+            return SueldoDiario * NumDias; // EL GET QUE RETORNA LA PROPIEDAD
+        }
+
+    }
+
+    
+
+}
---
+++ EncapsulamientoClases/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EncapsulamientoClases
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ClsEmpleado emple1 = new ClsEmpleado("luis", 33, 120.9f);
+
+            Console.WriteLine("su nomnre es " + emple1.Nombre);// get
+
+            emple1.Nombre = "Steven Universe";//set
+
+            //cambiar la edad, leer la edad y el sueldo diario
+
+            Console.WriteLine("dias trababajos?");
+            int dias = Convert.ToInt32(Console.ReadLine());
+
+            Console.WriteLine("Su sueldo va a ser: " + emple1.CalculaSueldo(dias));
+
+
+
+        }
+    }
+}
