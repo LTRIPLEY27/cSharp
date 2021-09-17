9696602f99a2f01fe765c29fd09379fae634e97e
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-17 20:19:36 +0200

    Array de Objetos

---
+++ PersonaEncapsulamiento/ClsPersona.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaEncapsulamiento
+{
+    class ClsPersona
+    {
+        private string _name;
+        private int _age;
+        private string _dni;
+        private char _sex;
+        private float _weigth;
+        private float _heigth;
+
+        public ClsPersona(string N, int A, string D, char S, float W, float H)
+        {
+            Name = N;  /// CON EL CONSTRUCTOR AUTOMATICO DE CSHARP SE HA DE SEGUIR ESTA SINTAXIS EN EL CONSTRUCTOR Y EN CUALQUIER ASPECTO DE LA CLASE!!!
+            Age = A;
+            Dni = D;
+            Sex = S;
+            Weigth = W;
+            Heigth = H;
+
+        }
+
+        public string Name { get => _name; set => _name = value; }
+        public int Age { get => _age; set => _age = value; }
+        public string Dni { get => _dni; set => _dni = value; }
+        public char Sex { get => _sex; set => _sex = value; }
+        public float Weigth { get => _weigth; set => _weigth = value; }
+        public float Heigth { get => _heigth; set => _heigth = value; }
+
+        public string calcularMC()
+        {
+            if(Weigth / Heigth < 20)
+            {
+                return "Estas en tu peso ideal";
+            }
+            if (Weigth / Heigth > 20 && Weigth / Heigth <= 25)
+            {
+                return "Es un peso correcto";
+            } else
+            {
+                return "Tienes sobrepeso";
+            }
+        }
+
+        public string esMayorDeEdad()
+        {
+            if(Age >= 18)
+            {
+                return "Es mayor de edad";
+            } else 
+            {
+                return "Es menor de edad";
+            }
+        }
+
+        public string toString()
+        {
+            return $"Ficha Tecnica \n  Nombre : {Name} \n Edad: {Age}  {esMayorDeEdad()} \n DNI: {Dni} \n Sexo: {Sex} \n Peso: {Weigth} \n Altura: {Heigth} \n \n El Indice de Masa Corporal segun su valoracion es : {calcularMC()}";
+        }
+    }
+}
---
+++ PersonaEncapsulamiento/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PersonaEncapsulamiento
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            //Stack persons = new Stack();
+
+            ClsPersona[] personas = new ClsPersona[3];
+
+            for(int i = 0; i < personas.Length; i++)
+            {
+
+                Console.WriteLine($"indique su nombre {i+1}");
+                string name = Console.ReadLine();
+
+                Console.WriteLine($"indique su edad {i+1}");
+                int edad = Convert.ToInt16(Console.ReadLine());
+
+                Console.WriteLine($"indique su DNI {i + 1}");
+                string dni = Console.ReadLine();
+
+                Console.WriteLine($"indique su sexo, con la inicial del mismo {i + 1}");
+                char sex = Convert.ToChar(Console.ReadLine());                
+                Console.WriteLine($"indique su peso{i + 1}");
+                float weight = (float)Convert.ToDouble(Console.ReadLine());
+
+                Console.WriteLine($"indique su altura {i + 1}");
+                float height = (float)Convert.ToDouble(Console.ReadLine());
+
+                personas[i] = new ClsPersona(name, edad, dni, sex, weight, height);
+
+            }
+
+            for(int i = 0; i < personas.Length; i++)
+            {
+                Console.WriteLine(personas[i].toString());
+            }
+        }
+    }
+}
