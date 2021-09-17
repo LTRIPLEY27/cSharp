744dfb6a80f0a83450ef302d94cbc397ae0dd4d3
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-17 17:46:07 +0200

    Encapsulamiento Facebook Ejercicio2

---
+++ Facebook/ClsFacebook.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook
+{
+    class ClsFacebook
+    {
+        private string _Name;
+        private int _Age;
+        private string _Password;
+
+        public ClsFacebook(string N, int A, string P)
+        {
+            this.Name = N;
+            this.Age = A;
+            this._Password = P;
+        }
+
+        public string Name { get => _Name; set => _Name = value; }
+        
+        public int Age { get => _Age; set => _Age = value; }
+        
+        
+        public string Password { get => _Password; set => _Password = value; }
+
+        public string toString()
+        {
+            return "Los datos registrados en su perfil de Facebook son \n Nombre: " + _Name + "\n Edad: " + this._Age + "\n Contraseña: " + Password;
+        }
+    }
+}
---
+++ Facebook/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Facebook
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Registro Nuevo usuario Facebook");
+
+            Console.WriteLine("Nombre: ");
+            string nom = Console.ReadLine();
+
+
+            Console.WriteLine("Edad: ");
+            int edad = Convert.ToInt32(Console.ReadLine());
+
+
+            Console.WriteLine("Contraseña: ");
+            string pass = Console.ReadLine();
+
+            ClsFacebook libro = new ClsFacebook(nom, edad, pass);
+
+            Console.WriteLine(libro.toString());
+
+
+
+        }
+    }
+}
