667cd5cc6f04068490e1fb3779d5acd771fa33b2
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-24 19:09:58 +0200

    Aplicaion Persona, USO DE DECLARACION DE OBJETOS A NIVEL GLOBAL SIN INICIALIZAR LA INSTANCIA  3

---
+++ Persona/ClsPerson.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Persona
+{
+    class ClsPerson
+    {
+        private string _Name;
+        private string _Dni;
+        private int _Age;
+
+        public ClsPerson(string Nombre, string DNI, int Edad)
+        {
+            Name = Nombre;
+            Dni = DNI;
+            Age = Edad;
+        }
+
+        public string Name { get => _Name; set => _Name = value; }
+        public string Dni { get => _Dni; set => _Dni = value; }
+        public int Age { get => _Age; set => _Age = value; }
+
+        public string toString()
+        {
+            return $"El registro del usuario es: \n \n Nombre {Name} \n Dni {Dni} \n Edad {Age}";
+        }
+    }
+
+    
+}
---
+++ Persona/Form1.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Persona
+{
+    public partial class Form1 : Form
+    {
+        ClsPerson person;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            person = new ClsPerson(txtName.Text, txtDni.Text, Convert.ToInt32(nudAge.Value));
+
+            btnInformation.Enabled = true;
+        }
+
+        private void btnInformation_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show(person.toString());
+        }
+    }
+}
