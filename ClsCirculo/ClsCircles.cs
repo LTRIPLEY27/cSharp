39be7c3fda9941285b4d7df3afb35e03ec3e413e
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-24 16:31:59 +0200

    clase ejercicio con forms 1

---
+++ ClsCirculo/ClsCircles.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClsCirculo
+{
+    class ClsCircles  // LA CLASE CIRCLES AL COMPARTIR EL "NAMESPACE" CON JAVA NO HACE NECESARIA EL LLAMADO SINO SOLO INSTANCIARLA Y LA RECONOCERA.
+    {
+
+        public int radio; // PROPIEDAD
+
+        /*public ClsCircles(float v)
+        {
+            this.radio = v;
+        }*/
+
+        public ClsCircles(int ra)
+        {
+            this.radio = ra;
+        }
+
+        public double CalcularArea()
+        {
+
+            return this.radio * this.radio * Math.PI;
+
+            // pi * (radio al cuadrado)
+        }
+
+        public double CalcularPerimetro()
+        {
+            return this.radio * 2 * Math.PI;
+        }
+    }
+
+}
+
---
+++ ClsCirculo/Form1.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClsCirculo
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void rbArea_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnCalcular_Click(object sender, EventArgs e)
+        {
+
+            // INSERSION DE LA CLASE CIRCLES EN EL EVENTO DEL BUTTON
+
+            ClsCircles circle = new ClsCircles((int)Convert.ToDouble(txtRadio.Text)); 
+
+            if(rbArea.Checked)
+            {
+                MessageBox.Show($"El area es: {Math.Round(circle.CalcularArea(), 2).ToString()} ");  // CLASE "MATH.ROUND" PARA ESPECIFICAR LA RESPUESTA EEN DECIMALES Y LA CANTIDAD DE LOS MISMOS
+            } else
+            {
+                MessageBox.Show($"El perimetro es : {Math.Round(circle.CalcularPerimetro(), 2).ToString()}");
+            }
+        }
+    }
+}
