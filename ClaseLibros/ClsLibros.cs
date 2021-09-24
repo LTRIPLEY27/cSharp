0baa7e74619b3ca93eca3b314dfcbca00bd511eb
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-09-24 18:35:44 +0200

    Proywcto Form clase libros con instancias globales, 2.

---
+++ ClaseLibros/ClsLibros.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClaseLibros
+{
+    class ClsLibros
+    {
+
+        private string _Title;
+        private string _Author;
+        private int _Examples;
+     
+
+        public ClsLibros(string Titulo, string Autor, int Ejemplar)
+        {
+            Title = Titulo;
+            Author = Autor;
+            Examples = Ejemplar;
+        }
+
+        
+
+        public string Title { get => _Title; set => _Title = value; }
+
+        public string Author { get => _Author; set => _Author = value; }
+
+        public int Examples { get => _Examples; set => _Examples = value; }
+
+        public string toString()
+        {
+            return $"El libro almacenado tiene los siguientes datos: \n \n Titulo: {Title} \n Autor: {Author} \n Ejemplar : {Examples}";
+        }
+    }
+}
---
+++ ClaseLibros/Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace ClaseLibros
+{
+    public partial class Form1 : Form
+    {
+
+        ClsLibros book; // DECLARACION DE LA CLASE EN EL AMBITO GLOBAL PARA PODER HACER USO DE LA MISMA POR TODOS LOS METODOS
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void txtTitle_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            book = new ClsLibros(txtTitle.Text, txtAuthor.Text, Convert.ToInt32(nuvExamples.Value));
+
+            //btnVerRegistro.Enabled = true;
+
+           // MessageBox.Show(book.toString());
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            
+            MessageBox.Show(book.toString());
+        }
+    }
+}
