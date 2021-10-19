c9e0915dc7f6b1b53a8f5aebe24b125e0441981e
isahellz calz <isabel.calzadilla.18@gmail.com>
2021-10-19 19:05:28 +0200

    version final

---
+++ ElJocDeLaVie/Form1.Designer.cs
@@ -0,0 +1,162 @@
+﻿
+namespace ElJocDeLaVie
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variable del diseñador necesaria.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén usando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido de este método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.btnRandom = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.tmrIteracion = new System.Windows.Forms.Timer(this.components);
+            this.btnStart = new System.Windows.Forms.Button();
+            this.btnPause = new System.Windows.Forms.Button();
+            this.lblTitle = new System.Windows.Forms.Label();
+            this.lblAuthor = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pictureBox1.BackColor = System.Drawing.Color.Fuchsia;
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.Fixed3D;
+            this.pictureBox1.Location = new System.Drawing.Point(60, 40);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(400, 400);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // btnRandom
+            // 
+            this.btnRandom.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnRandom.Location = new System.Drawing.Point(521, 40);
+            this.btnRandom.Name = "btnRandom";
+            this.btnRandom.Size = new System.Drawing.Size(75, 23);
+            this.btnRandom.TabIndex = 1;
+            this.btnRandom.Text = "Azar";
+            this.btnRandom.UseVisualStyleBackColor = true;
+            this.btnRandom.Click += new System.EventHandler(this.btnRandom_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnClose.Location = new System.Drawing.Point(521, 296);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 2;
+            this.btnClose.Text = "Cerrar";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.btnClose_Click);
+            // 
+            // tmrIteracion
+            // 
+            this.tmrIteracion.Interval = 500;
+            this.tmrIteracion.Tick += new System.EventHandler(this.tmrIteracion_Tick);
+            // 
+            // btnStart
+            // 
+            this.btnStart.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnStart.Location = new System.Drawing.Point(521, 120);
+            this.btnStart.Name = "btnStart";
+            this.btnStart.Size = new System.Drawing.Size(75, 23);
+            this.btnStart.TabIndex = 3;
+            this.btnStart.Text = "Iniciar";
+            this.btnStart.UseVisualStyleBackColor = true;
+            this.btnStart.Click += new System.EventHandler(this.btnStart_Click);
+            // 
+            // btnPause
+            // 
+            this.btnPause.Font = new System.Drawing.Font("Courier New", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnPause.Location = new System.Drawing.Point(521, 212);
+            this.btnPause.Name = "btnPause";
+            this.btnPause.Size = new System.Drawing.Size(75, 23);
+            this.btnPause.TabIndex = 4;
+            this.btnPause.Text = "Pausar";
+            this.btnPause.UseVisualStyleBackColor = true;
+            this.btnPause.Click += new System.EventHandler(this.btnPause_Click);
+            // 
+            // lblTitle
+            // 
+            this.lblTitle.AutoSize = true;
+            this.lblTitle.Font = new System.Drawing.Font("Courier New", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTitle.Location = new System.Drawing.Point(270, 9);
+            this.lblTitle.Name = "lblTitle";
+            this.lblTitle.Size = new System.Drawing.Size(257, 23);
+            this.lblTitle.TabIndex = 5;
+            this.lblTitle.Text = "El Juego de la vida";
+            this.lblTitle.Click += new System.EventHandler(this.lblTitle_Click);
+            // 
+            // lblAuthor
+            // 
+            this.lblAuthor.AutoSize = true;
+            this.lblAuthor.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.lblAuthor.Font = new System.Drawing.Font("David Libre", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblAuthor.ForeColor = System.Drawing.SystemColors.Menu;
+            this.lblAuthor.Location = new System.Drawing.Point(482, 387);
+            this.lblAuthor.Name = "lblAuthor";
+            this.lblAuthor.Size = new System.Drawing.Size(197, 15);
+            this.lblAuthor.TabIndex = 6;
+            this.lblAuthor.Text = "Desarrollado : Isabel Calzadilla";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblAuthor);
+            this.Controls.Add(this.lblTitle);
+            this.Controls.Add(this.btnPause);
+            this.Controls.Add(this.btnStart);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnRandom);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button btnRandom;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Timer tmrIteracion;
+        private System.Windows.Forms.Button btnStart;
+        private System.Windows.Forms.Button btnPause;
+        private System.Windows.Forms.Label lblTitle;
+        private System.Windows.Forms.Label lblAuthor;
+    }
+}
+
--- ElJocDeLaVie/Form1.cs
+++ ElJocDeLaVie/Form1.cs
@@ -13,8 +13,10 @@ namespace ElJocDeLaVie
     public partial class Form1 : Form
     {
         // ATRIBUTOS
-        private int longitude = 30;
+        private int longitude = 40;
         private int PixelLongitude = 10;
+
+        System.Drawing.Graphics x;
         int[,] celulas;
         public Form1()
         {
@@ -22,22 +24,25 @@ namespace ElJocDeLaVie
             celulas = new int[longitude, longitude];
         }
 
+
+        // VIVAS = 1
+        // MUERTAS = 0;
         private void btnRandom_Click(object sender, EventArgs e)
         {
             for(int i = 0; i < longitude; i++)
             {
                 for(int j = 0; j < longitude; j++)
                 {
-                    celulas[i, j] = 0;
+                    celulas[i,j] = 0;
                 }
             }
 
-            Random rand = new Random();
+            Random rand = new Random(); // GENERAMOS LOS VALORES DE 0 O 1 PARA RECONOCER SI ESTA VIVA O MUERTA
             for (int i = 0; i < longitude; i++)
             {
                 for (int j = 0; j < longitude; j++)
                 {
-                    celulas[i, j] = rand.Next(0,2);
+                    celulas[i,j] = rand.Next(0,2);
                 }
             }
 
@@ -52,16 +57,20 @@ namespace ElJocDeLaVie
         private void MatrizShow()
         {
             Bitmap bit = new Bitmap(pictureBox1.Width, pictureBox1.Height); // DIMENSIONES DE LA MATRIZ
+
+            Console.ForegroundColor = ConsoleColor.White;// ME DECLINE POR LA CLASE BIT
+
+
             for(int i = 0; i < longitude; i++)
             {
                 for(int j = 0; j < longitude; j++)
                 {
                     if(celulas[i,j] == 0)
                     {
-                        PaintPlace(bit, i, j, Color.Red);
+                        PaintPlace(bit, i, j, Color.White);
                     } else
                     {
-                        PaintPlace(bit, i, j, Color.Black);
+                        PaintPlace(bit, i, j, Color.PeachPuff);
                     }
                 }
             }
@@ -83,6 +92,141 @@ namespace ElJocDeLaVie
         private void btnClose_Click(object sender, EventArgs e)
         {
             this.Close();
+        }
+
+        private void btnStart_Click(object sender, EventArgs e)
+        {
+            tmrIteracion.Enabled = true; // INICIA AL OBJETO TIMER PARA QUE ITERE
+        }
+
+        private void btnPause_Click(object sender, EventArgs e)
+        {
+            tmrIteracion.Enabled = false; // PAUSA LOS MILISENGUDOS DEL TIMER CON EL "FALSE"
+        }
+
+
+        private void IniciaJuego()
+        {
+            int[,] celulasIniciales = new int[longitude, longitude];  // ESTADO INICIAL DEL JUEGO Y CON EL QUE ITERAREMOS
+            for (int i = 0; i < longitude; i++)
+            {
+                for (int j = 0; j < longitude; j++)
+                {
+                    if (celulas[i, j] == 0)
+                    {
+                        celulasIniciales[i,j] = VerificarEstado(i,j,false);  // CONDICIONAMOS AL METODO DE VERIFICACION EL LLENADO O NO DE LAS CASILLAS
+                    } else
+                    {
+                        celulasIniciales[i,j] = VerificarEstado(i,j,true);
+                    }
+                }
+            }
+
+            celulas = celulasIniciales;
+        }
+
+
+        public int VerificarEstado(int i,int j,bool IsAlive)
+        {
+            int CelulasVivas = 0; // INICIAMOS LA VARIABLE EN 0 PARA LLEVAR EL CONTEO DE SI ESTAN O NO 3 CELULAS VIVAS
+
+            if( i > 0 && j > 0)
+            {
+                if( celulas[i - 1, j - 1] == 1)
+                {
+                    CelulasVivas++;
+                }
+            }
+
+
+            if (j > 0)
+            {
+                if (celulas[i , j -1] == 1)
+                {
+                    CelulasVivas++;
+                }
+            }
+
+
+            if (i < longitude - 1 && j > 0) // CUBRIMOS LOS COSTADOS
+            {
+                if (celulas[i + 1, j - 1] == 1)
+                {
+                    CelulasVivas++;
+                }
+            }
+
+
+            if (i > 0)
+            {
+                if (celulas[i - 1, j] == 1)
+                {
+                    CelulasVivas++;
+                }
+            }
+
+
+            if (i < longitude - 1)  // BORDES
+            {
+                if (celulas[i + 1, j] == 1)
+                {
+                    CelulasVivas++;
+                }
+            }
+
+
+            if (i > 0 && j < longitude - 1)
+            {
+                if (celulas[i - 1, j + 1] == 1)
+                {
+                    CelulasVivas++;
+                }
+            }
+
+
+            if (j < longitude - 1)
+            {
+                if (celulas[i , j + 1] == 1)
+                {
+                    CelulasVivas++;
+                }
+            }
+
+
+            if (i < longitude - 1 && j < longitude - 1)
+            {
+                if (celulas[i + 1, j + 1] == 1)
+                {
+                    CelulasVivas++;
+                }
+            }
+
+            // CONDICIONAL A VERIFICAR SI EL ESTADO ES VIVA O MUERTA SEGUN EL CONTEO
+
+            if(IsAlive)
+            {
+                return (CelulasVivas == 2 || CelulasVivas == 3) ? 1 : 0; // TERNARIO PARA SABER SI ES KAPUT
+            } else
+            {
+                return CelulasVivas == 3 ? 1 : 0;
+            }
+            
+        }
+
+        private void tmrIteracion_Tick(object sender, EventArgs e)
+        {
+            IniciaJuego();
+            MatrizShow();
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblTitle_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
